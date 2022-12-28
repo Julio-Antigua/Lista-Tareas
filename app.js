6cd2726d17cafcaf753787c6bc6c3ae1b0b3bc47
Julio Saul <juliosaul12@gmail.com>
2022-12-28 12:54:27 -0400

    - Se creo el switch para las funciones del menu.

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-import { inquirerMenu } from './helpers/inquirer.js';
+import { inquirerMenu, inquirerPausa, leerInput } from './helpers/inquirer.js';
+import { Tareas } from './models/tareas.js';
 
 console.clear();
 
 const main = async() => {
     let opt = '';
+    const tareas = new Tareas();
 
     do
     {
+        // Impprimir el menu
         opt = await inquirerMenu();
-        console.log({ opt });
+        switch (opt){
+            case '1':
+                // crear opcion
+                const desc = await leerInput('Descripcion:');
+                tareas.crearTarea(desc);
+                break;
+            case '2':
+                console.log( tareas._listado);
+                break;
+        }
+
+        await inquirerPausa();
     }while(opt !== '0');
 
 }
