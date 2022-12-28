1e26385feaa6184928fe91ba88fcb765a2526955
Julio Saul <juliosaul12@gmail.com>
2022-12-28 12:55:44 -0400

    - Se creo la funcion inquirerPausa y leerInput. - - Se cambio el color de los numeros del menu. -

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -6,7 +6,45 @@ const preguntas = [
         type: 'list',
         name: 'opcion',
         message: '¿Que desea hacer?',
-        choices: ['opt1','opt2','opt3']
+        choices: [
+            // 'opt1','opt2','opt3'
+            {
+                value: '1',
+                name: `${'1.'.green} Crear tarea`
+            },
+            {
+                value: '2',
+                name: `${'2.'.green} Listar tareas`
+            },
+            {
+                value: '3 ',
+                name: `${'3.'.green} Listar tarea completadas`
+            },
+            {
+                value: '4 ',
+                name: `${'4.'.green} Listar tareas pendientes`
+            },
+            {
+                value: '5',
+                name: `${'5.'.green} Completar tareas(s)`
+            },
+            {
+                value: '6',
+                name: `${'6.'.green} Borrar tarea`
+            },
+            {
+                value: '0',
+                name: `${'0.'.green} Salir`
+            },
+        ]
+    }
+]
+
+const pausa = [
+    {
+        type: 'input',
+        name: 'enter',
+        message: `Presione ${ 'ENTER'.green } para continuar`
     }
 ]
 
@@ -14,14 +52,38 @@ const inquirerMenu = async() => {
 
     console.clear();
     console.log('=============================='.green);
-    console.log('   Seleccione una opcion   '.green);
+    console.log('   Seleccione una opcion   '.white);
     console.log('==============================\n'.green);
 
-    const opt = await inquirer.prompt(preguntas);
-    return opt;
+    const { opcion } = await inquirer.prompt(preguntas);
+    return opcion;
 
 };
 
+const inquirerPausa = async() => {
+    console.log('\n')
+    await inquirer.prompt(pausa);
+}
+
+const leerInput = async(mensaje) => {
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message: mensaje,
+            validate(value){
+                if(value.length === 0){
+                    return 'Por favor ingrese un valor';
+                }
+                return true;
+            }
+        }
+    ];
+
+    const {desc} = await inquirer.prompt(question);
+    return desc;
+}
+
 export{
-    inquirerMenu
+    inquirerMenu, inquirerPausa,leerInput
 }
